536630a660a6244ca04b2768560368f8b47de948
gokuldeepak <s.gokuldeepak@gmail.com>
2023-02-19 17:32:02 +0530

    data

--- python1/data.tf
+++ python1/data.tf
@@ -4,6 +4,8 @@ data "terraform_remote_state" "infra" {
           bucket = "gokuldeepak"
           key    = "statefiles/infra.tfstate"
           region = "us-east-1"
+          access_key = var.AWS_ACCESS_KEY_ID
+          secret_key = var.AWS_SECRET_ACCESS_KEY
     }
 }
 
